a501faf98c83b17043258e7c001ef42c5337bfec
mdriyadtalukder <riyadtalukder67@gmail.com>
2025-07-09 18:30:06 +0600

    added

--- starting/_4strings/Strings.java
+++ starting/_4strings/Strings.java
@@ -50,7 +50,9 @@ public class Strings {
         System.out.println(s.indexOf("iy", 2)); // index 2 theke i khuja suru krbe
         System.out.println(s.lastIndexOf("i")); // last i er index num ber kre
         System.out.println(s.lastIndexOf("i", 2)); // 0-2 porjnto index check kre khujbe..
-        System.out.println(s.concat(ss)); // s o ss jug kre ba jura lagai..
+        System.out.println(s.concat(ss)); // s o ss jug kre ba jura lagai....print hbe but print na kre emn kre then s
+                                          // print krle hbe na.,kono string er vitor rakhte hbe.na hy just emne print
+                                          // krte hbe..
         System.out.println(s.charAt(1)); // 1 no index er character ber kre
         System.out.println(s.codePointAt(0)); // 0 index er value er ASCII value dibe..
         System.out.println(s.isEmpty());
@@ -66,6 +68,17 @@ public class Strings {
         for (String i : q) {
             System.out.println(i);
         }
+
+        String sc = "abe", c = "abc"; // c boro coz a theke b boro...abc vs abe..abe boro coz last er c er theke e
+                                      // boro..
+
+        System.out.println(sc.compareTo(c)); // sc=c hole 0 dibe,sc boro hole positive num dibe r choto hole negative
+                                             // num dibe
+        // output 2 ekhne..coz c theke e ta 2 ghor por like d,e..sc choto hole -2 hoto..
+
+        System.out.println("abc" + 10 + 20); // abc1020..abc+(10+20) dile 30 hbe....string+number
+        System.out.println(10 + 20 + "abc");// 30abc..number+string
+        System.out.println("abc" + 'k'); // string+char
 
     }
 }
--- starting/_4strings/StringsBuilders.java
+++ starting/_4strings/StringsBuilders.java
@@ -1,18 +1,30 @@
 public class StringsBuilders {
     public static void main(String[] args) {
 
-        // total 3 methods
+        // total 10 methods
 
         /*
          * 1.add
          * 2.remove
          * 3.reverse
+         * 4.setCharAt
+         * 5. insert
+         * 6. delete at
+         * 7.substring..9.with 2 index
+         * 8.indexof..10.with 2 index
          */
-        StringBuffer s = new StringBuffer("Ami");
+        StringBuilder s = new StringBuilder("Ami");
         System.out.println(s.append(" Riyad"));
+        s.setCharAt(0, 't');
         System.out.println(s.append(122));
         System.out.println(s.append(134.5555));
         System.out.println(s.reverse());
         System.out.println(s.delete(0, 5)); // 5-1=4..0-4 delete krbe
+
+        StringBuilder m = new StringBuilder("tmi");
+        System.out.println(m.insert(1, 'u')); // 1 er ta 2 hbe and 1 e new char dibe..not replace..
+        m.deleteCharAt(1);
+        System.out.println(m);
+
     }
 }
